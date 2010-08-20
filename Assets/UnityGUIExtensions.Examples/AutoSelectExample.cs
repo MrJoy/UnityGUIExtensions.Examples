b7022fbf21ea5249e78c794d10943a99cfd2ebb2
Jon Frisby <jfrisby@mrjoy.com>
2010-08-20 14:49:39 -0500

    Mucking about with rich text rendering.  Not working yet.

--- Assets/UnityGUIExtensions.Examples/AutoSelectExample.cs
+++ Assets/UnityGUIExtensions.Examples/AutoSelectExample.cs
@@ -4,12 +4,16 @@ using System.Collections;
 
 public class AutoSelectExample : MonoBehaviour {
   private string a = "", b = "", c = "";
+  public Font normalFont, boldFont, italicFont;
 
   public void OnGUI() {
-   GUILayout.BeginArea(new Rect(0, 0, 400, 200));
+    GUILayout.BeginArea(new Rect(0, 0, 400, 200));
       a = GUILayoutExt.AutoSelectTextArea("fieldA", a);
       b = GUILayoutExt.AutoSelectTextArea("fieldB", b);
       c = GUILayout.TextArea(c);
-   GUILayout.EndArea();
+    GUILayout.EndArea();
+    GUILayout.BeginArea(new Rect(0, 210, 400, 500));
+      GUICommon.FancyLabel(new Rect(0, 0, 400, 500), c, GUI.skin.label, normalFont, boldFont, italicFont, TextAlignment.Left);
+    GUILayout.EndArea();
   }
 }
