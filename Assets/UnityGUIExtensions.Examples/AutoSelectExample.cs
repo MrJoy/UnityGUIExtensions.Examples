3838fce52c49e89ca62edb3dc2f89d430897d654
Jon Frisby <jfrisby@mrjoy.com>
2010-11-28 12:17:11 -0800

    Formatting correction.

--- Assets/UnityGUIExtensions.Examples/AutoSelectExample.cs
+++ Assets/UnityGUIExtensions.Examples/AutoSelectExample.cs
@@ -6,10 +6,10 @@ public class AutoSelectExample : MonoBehaviour {
   private string a = "", b = "", c = "";
 
   public void OnGUI() {
-   GUILayout.BeginArea(new Rect(0, 0, 400, 200));
+    GUILayout.BeginArea(new Rect(0, 0, 400, 200));
       a = GUILayoutExt.AutoSelectTextArea("fieldA", a);
       b = GUILayoutExt.AutoSelectTextArea("fieldB", b);
       c = GUILayout.TextArea(c);
-   GUILayout.EndArea();
+    GUILayout.EndArea();
   }
 }
