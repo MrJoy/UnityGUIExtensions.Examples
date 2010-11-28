5522f8d9e47e6ecfb5e74496042cefc959138af8
Jon Frisby <jfrisby@mrjoy.com>
2010-11-28 12:25:14 -0800

    Add some documentation.

--- Assets/UnityGUIExtensions.Examples/AutoSelectExample.cs
+++ Assets/UnityGUIExtensions.Examples/AutoSelectExample.cs
@@ -7,9 +7,19 @@ public class AutoSelectExample : MonoBehaviour {
 
   public void OnGUI() {
     GUILayout.BeginArea(new Rect(0, 0, 400, 200));
-      a = GUILayoutExt.AutoSelectTextArea("fieldA", a);
-      b = GUILayoutExt.AutoSelectTextArea("fieldB", b);
-      c = GUILayout.TextArea(c);
+      GUILayout.BeginHorizontal();
+        GUILayout.Label("Auto-selecting text field:", GUILayout.Width(140));
+        a = GUILayoutExt.AutoSelectTextArea("fieldA", a);
+      GUILayout.EndHorizontal();
+      GUILayout.BeginHorizontal();
+        GUILayout.Label("Auto-selecting text field:", GUILayout.Width(140));
+        b = GUILayoutExt.AutoSelectTextArea("fieldB", b);
+      GUILayout.EndHorizontal();
+      GUILayout.BeginHorizontal();
+        GUILayout.Label("Plain text field:", GUILayout.Width(140));
+        c = GUILayout.TextArea(c);
+      GUILayout.EndHorizontal();
+      GUILayout.Label("The auto-selecting text fields above will automatically select their contents when they gain focus, so by default the user will be able to replace the contents without having to erase them explicitly.");
     GUILayout.EndArea();
   }
 }
